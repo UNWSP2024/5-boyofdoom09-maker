7b76cc8f3330913444e8ffccf88c06ba64fec89b
boyofdoom09-maker <boyofdoom09@outlook.com>
2025-10-03 13:09:36 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -6,7 +6,13 @@
 # + 129
 
 # ------
+answer = int(input('145 + 255 = '))
+if answer == 300:
+    print('That is correct!')
+else:
+    print('That is incorrect the correct answer is 300')
 
+    
 # The program should allow the student to enter the answer.  
 # If the answer is correct, a message of congratulations should be displayed.  
 # If the answer is incorrect a message showing the correct answer should be displayed.  
